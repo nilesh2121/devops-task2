0cf545145339a15ef283321fb2df360cd3332c4f
nilesh2121 <nilesh.koli@gmail.com>
2022-07-18 19:38:22 +0530

    added the key location

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "webserver" {
     ami = "ami-08d4ac5b634553e16"
     instance_type = "t2.micro"
-    key_name = "mylaptop-us"
+    key_name = "mylaptop"
     subnet_id = data.aws_subnet.public-subnet.id
     associate_public_ip_address = true
     vpc_security_group_ids = [aws_security_group.websg.id]
@@ -74,7 +74,7 @@ resource "aws_instance" "webserver" {
 resource "aws_instance" "dbserver" {
     ami = "ami-08d4ac5b634553e16"
     instance_type = "t2.micro"
-    key_name = "mylaptop-us"
+    key_name = "mylaptop"
     subnet_id = aws_subnet.private_subnet.id
     vpc_security_group_ids = [aws_security_group.dbsg.id]
     tags = {
@@ -104,7 +104,7 @@ resource "aws_instance" "dbserver" {
 # added the keypaire location - production
 
 resource "aws_key_pair" "mylaptop-us" {
-    key_name = "mylaptop-us"
+    key_name = "mylaptop"
     public_key = file("/home/devops/Key/.ssh/id_rsa.pub")
     
 }
