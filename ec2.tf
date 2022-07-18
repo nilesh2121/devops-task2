a13d87d4bb50b26425999f887a72f3fefa71712f
nilesh2121 <nilesh.koli@gmail.com>
2022-07-18 19:31:39 +0530

    added the key location

--- ec2.tf
+++ ec2.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "webserver" {
       type         = "ssh"
       host        = aws_instance.webserver.public_ip
       user        = "ubuntu"
-      private_key = var.priv_key
+      private_key = file(var.priv_key)
       timeout     = "4m"
     } 
 
@@ -105,7 +105,7 @@ resource "aws_instance" "dbserver" {
 
 resource "aws_key_pair" "mylaptop-us" {
     key_name = "mylaptop-us"
-    public_key = var.keypath
+    public_key = file(var.keypath)
     
 }
 
