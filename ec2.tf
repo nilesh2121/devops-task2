68005bf7799164dab9e92739bc3f1371aff52578
nilesh2121 <nilesh.koli@gmail.com>
2022-07-19 19:50:28 +0530

    added the hostkey

--- ec2.tf
+++ ec2.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "webserver" {
       type        = "ssh"
       host        = aws_instance.webserver.public_ip
       user        = "ubuntu"
-      private_key = var.priv_key
+      private_key = file(var.priv_key)
       timeout     = "4m"
     } 
 
