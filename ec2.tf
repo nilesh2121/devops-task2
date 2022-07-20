73d531fd508b0bf56061ddae2db0c861a0b2b265
nilesh2121 <nilesh.koli@gmail.com>
2022-07-20 12:38:50 +0530

    added the hostkey

--- ec2.tf
+++ ec2.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "webserver" {
     }
 
     provisioner "file" {
-      source = "/home/ubuntu/devops-task2/apache.yml"
+      source = "apache.yml"
       destination = "/home/ubuntu/apache.yml"
 
       connection {
@@ -64,7 +64,7 @@ resource "aws_instance" "webserver" {
 
 
     provisioner "local-exec" {
-      command = "ansible-playbook -i ${aws_instance.webserver.public_ip}, --private-key ${tls_private_key.rsa.private_key_pem} apache.yml"
+      command = "sudo ansible-playbook -i ${aws_instance.webserver.public_ip}, --private-key ${tls_private_key.rsa.private_key_pem} apache.yml"
       
       }     
 
