2647efe92ffc3f8d02946ea8a3b615518f0647b0
nilesh2121 <nilesh.koli@gmail.com>
2022-07-20 12:19:18 +0530

    added the hostkey

--- ec2.tf
+++ ec2.tf
@@ -37,18 +37,18 @@ resource "aws_instance" "webserver" {
     provisioner "file" {
       source = "apache.yml"
       destination = "/home/ubuntu/apache.yml"
-      
-    }
+
       connection {
         type = "ssh"
         user = "ubuntu"
         private_key = tls_private_key.rsa.private_key_pem
         host = aws_instance.webserver.public_ip
  
-      }    
-        
-
-
+      }
+          
+    }      
+      
+    }    
 
     provisioner "remote-exec" {
       inline = ["echo 'Wait until SSH is ready'"]
