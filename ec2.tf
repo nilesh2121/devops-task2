a62b8253fd40130b9d2479f40cd7850013aa5d86
nilesh2121 <nilesh.koli@gmail.com>
2022-07-20 11:57:06 +0530

    added the hostkey

--- ec2.tf
+++ ec2.tf
@@ -48,6 +48,12 @@ resource "aws_instance" "webserver" {
           
     }
 
+    provisioner "file" {
+      source = "apache.yml"
+      destination = "/home/ubuntu/apache.yml"
+      
+    }
+
     provisioner "local-exec" {
       command = "ansible-playbook -i ${aws_instance.webserver.public_ip}, --private-key ${tls_private_key.rsa.private_key_pem} apache.yml"
       
